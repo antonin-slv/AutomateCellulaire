ea4bee0dfb7ee90ae56fb6cee8575b658de8b04d
Tibouyou <tibouyou01@gmail.com>
2024-01-30 17:11:00 +0100

    Squelette

---
+++ code/src/Automate.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+
+public class Automate {
+    private int dim;
+    private int[] Q;
+    private Map<Integer,Map<String, Map<String,Integer>>> rules;
+    private int[][] voisinage;
+
+    public Automate(int dim, int[] Q, Map<Integer,Map<String, Map<String,Integer>>> rules, int[][] voisinage) {
+        this.dim = dim;
+        this.Q = Q;
+        this.rules = rules;
+        this.voisinage = voisinage;
+    }
+
+    public int sigma(int[] voisinage, int etat) {
+        //Todo : get the next state of the cell at the given coordinates
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public int[] getCoordsVoisinage() {
+        //Todo : get the neighborhood of the cell at the given coordinates
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ code/src/Moteur.java
@@ -0,0 +1,14 @@
+public class Moteur {
+    private Automate automate;
+    private Grille grille;
+
+    public void update() {
+        //Todo : update the grid
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void print() {
+        //Todo : print the grid
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
