65bf779410d251c87f285db81c2586985cd8bd75
Thomas Blanché <totodelaforet69@gmail.com>
2024-01-30 19:46:01 +0100

    instanciation des classes

--- code/src/Moteur.java
+++ code/src/Moteur.java
@@ -2,6 +2,12 @@ public class Moteur {
     private Automate automate;
     private Grille grille;
 
+    public Moteur(String rules_path) {
+
+        this.automate = new Automate(rules_path);
+        this.grille = new Grille(automate.getDim(), 20);
+    }
+
     public void update() {
         //Todo : update the grid
         throw new UnsupportedOperationException("Not implemented yet");
