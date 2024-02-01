b36802a8411e4abc6fcb554a74f409d00bbdc76c
Thomas Blanché <totodelaforet69@gmail.com>
2024-02-01 22:40:09 +0100

    Init Javafx
    Co-authored-by: uku <hi@uku.moe>
    Co-authored-by: Antonin Slv <antonin.sylvestre@orange.fr>
    Co-authored-by: Tibouyou <tibouyou01@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,24 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import rules.GameRule;
 import rules.RuleDeserializer;
 
+import javax.tools.SimpleJavaFileObject;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
 
-public class Main {
+public class Main extends Application {
     public static final Gson GSON = new GsonBuilder()
             .registerTypeAdapter(new TypeToken<GameRule>() {}.getType(), new RuleDeserializer())
             .create();
+
     public static void main(String[] args) throws IOException {
         URL rulesPath = Main.class.getClassLoader().getResource("rules/Major.json");
         URL gridPath = Main.class.getClassLoader().getResource("grids/grid1.json");
@@ -20,5 +27,19 @@ public class Main {
         moteur.print();
         moteur.update();
         moteur.print();
+
+        launch(); // lancer javafx
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("javafx/scene.fxml"));
+
+        Scene scene = new Scene(root);
+        // scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
     }
 }
